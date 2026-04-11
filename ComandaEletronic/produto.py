929707920919331412e316418c7dcc636f659450
lucas <lucasandre.seabra1@gmail.com>
2026-04-11 14:41:49 -0300

    Questões lista 01 APS.1

--- ComandaEletronic/produto.py
+++
@@ -1,7 +0,0 @@
-class Produto:
-    def __init__(self, nome: str, valor_unitario: float):
-        self.nome = nome
-        self.valor_unitario = valor_unitario
-
-    def exibir_produto(self) -> str:
-        return f"{self.nome} - R$ {self.valor_unitario:.2f}"
